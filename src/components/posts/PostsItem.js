9c06a77eaa95891facafab7db23abf71303eea3e
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2021-02-20 12:35:13 +0100

    johan

--- src/components/posts/PostsItem.js
+++ src/components/posts/PostsItem.js
@@ -4,18 +4,17 @@ import { Link } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const PostItem = ({ post }) => {
-  if (post.mainImage) {
-    const imagePost =
-      post.mainImage?.asset.localFile.childImageSharp.gatsbyImageData;
-    const { alt } = post.mainImage;
-  }
+  const imagePost =
+    post.mainImage?.asset.localFile.childImageSharp.gatsbyImageData;
+
+  const alt = post.mainImage?.alt;
 
   return (
     <>
       <h3>
         <Link to={post.slug.current}>{post.title}</Link>
       </h3>
-      {imagePost && <GatsbyImage alt={alt} image={imagePost} />}
+      {imagePost && alt ? <GatsbyImage alt={alt} image={imagePost} /> : ''}
     </>
   );
 };
